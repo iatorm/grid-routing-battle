23a347e88a467939c43f3bd1fb4d23c4b17a8367
Ilkka Törmä <iatorm@utu.fi>
2015-02-17 19:23:10 +0200

    Removed debug information from RandomBuilder.

--- bots/randombuilder/random_builder.py
+++ bots/randombuilder/random_builder.py
@@ -1,14 +1,12 @@
 import random as r
-import sys
 
 while True:
     msg = input().split()
-    sys.stderr.write(" ".join(msg)+'\n')
     if msg[0] == "BEGIN":
         side_len = int(msg[3])
     elif msg[0] == "DESTROY":
         print("NONE")
     elif msg[0] == "ACTIVATE":
-        print("VERTEX " + str(r.randrange(side_len)) + "," + str(r.randrange(side_len)))
+        print("VERTEX %d,%d"%(r.randrange(side_len), r.randrange(side_len)))
     elif msg[0] == "SCORE":
         break
